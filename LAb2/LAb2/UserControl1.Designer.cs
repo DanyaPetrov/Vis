013e298346aceedc6462a9b8f13c5ba45a2556a6
Shadow_F <121252067+DanyaPetrov@users.noreply.github.com>
2025-11-05 21:39:52 +0300

    Add files via upload

---
+++ LAb2/LAb2/UserControl1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace LAb2
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(205, 206);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pictureBox);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(684, 546);
+            this.Load += new System.EventHandler(this.UserControl1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
---
+++ LAb2/LAb2/UserControl1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Configuration;
+using System.Security.Cryptography;
+
+namespace LAb2
+{
+    public partial class UserControl1: UserControl
+    {
+        List<Point> points = new List<Point>();
+        Point startPoint = new Point();
+
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
+            Graphics g = Graphics.FromImage(bitmap);
+            /* Pen n = new Pen(Color.Black);
+             float[] x = { 10, pictureBox.Width/2, pictureBox.Width-10 };
+             float[] y = { 200f, 0, 200f };
+
+             PointF[] p = new PointF[x.Length];
+
+             for (int i = 0; i < x.Length; i++)
+                 p[i] = new PointF(x[i], y[i]);
+
+             g.DrawPolygon(n,p);
+            */
+            Draw(500);
+           /*foreach (var point in points)
+            {
+                bitmap.SetPixel(point.X, point.Y, point.Color);
+            }*/
+           
+
+
+           /* for (int i = 0; i < x.Length; i++)
+            {
+                brush = new SolidBrush(points[i].Color);
+                g.FillPolygon(brush, new PointF[] { p[i], p[(i + 1) % p.Length], p[(i + 2) % p.Length] });
+            }*/
+            pictureBox.Image = bitmap;
+        }
+        public void Draw(int width)
+        {
+            int hight = (int)((width / 2) * Math.Sqrt(3));
+            double xright = width;
+            double xleft = 0;
+
+            for (int i = hight; i > 0; i--)
+            {
+                for (int j = (int)xleft; j < xright; j++)
+                {
+                    points.Add(new Point(j, i, startPoint.ColorPoint(j, i, hight, xleft, xright)));
+                }
+
+                double temp = (i * 2) / Math.Sqrt(3.0);
+                double tmp = width - temp;
+
+                xright = width - (tmp / 2);
+                xleft = tmp / 2;
+            }
+        }
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    internal class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Color Color { get; set; }
+
+        public Point() { }
+
+        public Point(int x, int y, Color color)
+        {
+            X = x;
+            Y = y;
+            Color = color;
+        }
+
+        public Color ColorPoint(int x, int y, int height, double xleft, double xright)
+        {
+
+            double green = height - y;
+            double red = (x - xleft) * Math.Sqrt(3.0) / 2;
+            double blue = (xright - x) * Math.Sqrt(3.0) / 2;
+
+            double R = 255.0 / height;
+            double G = 255.0 / height;
+            double B = 255.0 / height;
+
+            return Color.FromArgb((int)(R * red), (int)(G * green), (int)(B * blue));
+        }
+    }
+}
+
+
+ 
