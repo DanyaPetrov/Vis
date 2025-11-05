64a93997ac8a78ec765d18b7ecb0a1a634f3cd44
Shadow_F <121252067+DanyaPetrov@users.noreply.github.com>
2025-11-05 22:16:47 +0300

    Add files via upload

---
+++ PR1/PR1/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace PR1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button13 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 75);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Свет";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(2, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 89);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Температура";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(2, 162);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 79);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Воздух";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(102, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(357, 161);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Тут должно быть фото с информацией";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(102, 159);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(111, 79);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Регулировка расхода энергии на первом этаже";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(210, 159);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(110, 79);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Регулировка расхода энергии на втором этаже";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(317, 159);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(142, 79);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Регулировка расхода энергии на третьем этаже";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button10);
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Location = new System.Drawing.Point(462, 142);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(106, 96);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Первый этаж";
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(0, 65);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(106, 23);
+            this.button10.TabIndex = 2;
+            this.button10.Text = "Уборная";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(0, 43);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(106, 23);
+            this.button9.TabIndex = 1;
+            this.button9.Text = "Гостинная";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(0, 20);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(106, 23);
+            this.button8.TabIndex = 0;
+            this.button8.Text = "Кухня";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button12);
+            this.groupBox2.Controls.Add(this.button11);
+            this.groupBox2.Location = new System.Drawing.Point(462, 65);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(106, 71);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Второй этаж";
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(0, 42);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(106, 23);
+            this.button12.TabIndex = 1;
+            this.button12.Text = "Уборная";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(0, 20);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(106, 23);
+            this.button11.TabIndex = 0;
+            this.button11.Text = "Спальня 1";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button13);
+            this.groupBox3.Location = new System.Drawing.Point(462, 0);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(106, 59);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Третий этаж";
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(0, 20);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(106, 39);
+            this.button13.TabIndex = 0;
+            this.button13.Text = "Кладовая";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 237);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button13;
+    }
+}
+
